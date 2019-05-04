3328df697deaa2582892319ee703340d8a14cd5b
levan563 <mrlevan563@gmail.com>
2019-05-04 12:48:54 +0300

    Added icons, scripts, animations

---
+++ Assets/Scripts/UITransitionButton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class UITransitionButton : MonoBehaviour, IPointerClickHandler
+{
+    private UIWindow current;
+    [SerializeField]
+    private UIWindow next;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        next.IsOpened = current.IsOpened;
+        current.IsOpened = !current.IsOpened;
+
+        //next.IsOpened = !(current.IsOpened = !current.IsOpened);
+    }
+
+    void Awake()
+    {
+        current = GetComponentInParent<UIWindow>();
+    }
+}
---
+++ Assets/Scripts/UIWindow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIWindow : MonoBehaviour {
+
+    private Animator animator;
+
+    public bool IsOpened
+    {
+        get { return animator.GetBool("isOpened"); }
+        set
+        {
+            if (!gameObject.activeInHierarchy) gameObject.SetActive(true);
+            if (value) transform.SetAsFirstSibling();
+            animator.SetBool("isOpened", value);
+        }
+    }
+
+    void OnEnable()
+    {
+        if (animator) return;
+        animator = GetComponent<Animator>();
+    }
+}
